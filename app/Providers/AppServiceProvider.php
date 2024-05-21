a6d907e91abe6e093a1211a0b77102d04a7d697f
Chimuka Moonde <chimukamoonde@gmail.com>
2024-05-21 10:03:26 +0200

    password expired

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//        if (App::isProduction()){
+//            \URL::forceScheme('https');
+//        }
+        Paginator::useBootstrap();
+        Schema::defaultStringLength(191);
     }
 }
